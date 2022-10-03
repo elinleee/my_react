96ae3158e275ab43cea0a5d0c5c91a04e25949f6
Youngji Lee <elin@Youngjis-MacBook-Pro.local>
2022-10-03 22:57:35 +0900

    set footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 import { Box, Grid, Paper } from '@mui/material';
 
 import './Footer.css';
-import { faBorderNone } from '@fortawesome/free-solid-svg-icons';
+import { FooterItem } from './FooterItem';
 
 function Footer(props) {
   const Item = styled(Paper)(({ theme }) => ({
@@ -16,63 +16,28 @@ function Footer(props) {
   }));
 
   return (
-    <Box className="footer" width="100vw" height="200px">
-      <Grid container spacing={2} width="720px" margin="0 auto">
-      <Grid item xs={4}>
-          <Item>
-          <h5>예배 실황</h5>
-              <p>
-                <a href='#!' className='text-reset'>
-                  주일예배
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  수요예배
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  쉴하쉬림
-                </a>
-              </p>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <h5>예배 실황</h5>
-              <p>
-                <a href='#!' className='text-reset'>
-                  주일예배
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  수요예배
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  쉴하쉬림
-                </a>
-              </p>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <h5>CONTACT</h5>
-              <p>
-                <a href='#!' className='text-reset'>
-                  서울특별시 강동구 천호대로 1212-6 석천교회
-                </a>
-              </p>
-              <p>
-                <a href='https://www.youtube.com/channel/UCJUsz3WJ3aLVO1vMEDIDsEQ' className='text-reset'>
-                 하늘가족석천교회
-                </a>
-              </p>
-          </Item>
-        </Grid>
+    <Box className="footer">
+      <Grid container spacing={4} width='80vw' margin="0 auto" justifyContent='space-around'>
+        {FooterItem.map((category, categoryIndex) => {
+          return (
+            <Grid item xs={category.size} key={categoryIndex}>
+              <Item className="footer-category" sx={{backgroundColor:'transparent', boxShadow:'none'}}>
+                <h5 className={category.cName}>{category.title}</h5>
+                <ul className="footer-ul">
+                {category.items.map((item, itemIndex) => {
+                  return (
+                    <li key={itemIndex}>
+                      <a href={item.extraUrl?item.extraUrl:null} className={item.cName} target={item.extraUrl?"_blank":""}>
+                        {item.title}
+                      </a>
+                    </li>
+                  );
+                })}
+                </ul>
+              </Item>
+            </Grid>
+          );
+        })}
       </Grid>
     </Box>
   );
---
+++ src/layouts/FooterItem.js
@@ -0,0 +1,58 @@
+export const FooterItem = [
+    {
+        title: '환영해요',
+        url: 'about',
+        size: 5,
+        cName: 'footer-links',
+        items: [
+            {
+                title: '샬롬! 석천을 찾아오신 모든 분들을 환영합니다. 쉼, 회복, 공동체가 있는 석천에서 함께 해요.',
+                url: 'about',
+                cName: 'footer-item-links',
+            }
+        ]
+    },
+    {
+        title: 'CONTACT',
+        url: null,
+        size: 3,
+        cName: 'footer-links',
+        items: [
+            {
+                title: '서울특별시 강동구 천호대로 1212-6 석천교회',
+                url: null,
+                extraUrl: 'https://naver.me/xScSmon0',
+                cName: 'footer-item-links',
+            },
+            {
+                title: 'Youtube 하늘가족석천교회',
+                url: null,
+                extraUrl: 'https://www.youtube.com/channel/UCJUsz3WJ3aLVO1vMEDIDsEQ',
+                cName: 'footer-item-links',
+            },
+        ]
+    },
+    {
+        title: '석천의 예배',
+        url: 'sermon',
+        size: 2,
+        cName: 'footer-links',
+        items: [
+            {
+                title: '주일 예배',
+                url: 'sermon',
+                cName: 'footer-item-links',
+            },
+            {
+                title: '수요 예배',
+                url: 'sermon',
+                cName: 'footer-item-links',
+            },
+            {
+                title: '쉴하쉬림',
+                url: 'sermon',
+                cName: 'footer-item-links',
+            },
+        ]
+    },
+]
